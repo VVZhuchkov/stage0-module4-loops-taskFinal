4afa6353d92b5610dc6d71e79ec240a493658cb1
VVZhuchkov <vzhuchkov@tut.by>
2022-10-31 23:12:24 +0300

    Done

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,11 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        int number = 1;
+        for (int i = 0; i < power; i++){
+            number = number*numberToPrint;
+        }
+        System.out.println(number);
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,20 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j < cathetusLength + i; j++) {
+                if ((j > cathetusLength - i) && (j<cathetusLength+i) && (j<=cathetusLength)) {
+                        System.out.print(cathetusLength-j+1);
+                }
+                else if ((j > cathetusLength - i) && (j<cathetusLength+i) && (j>cathetusLength)) {
+                    System.out.print(j-cathetusLength+1);
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
